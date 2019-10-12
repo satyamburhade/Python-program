7d95511221392c26e63c1421e77ffe55f647712c
Satyam Dipak Burhade <sdb8275@gmail.com>
2019-10-12 22:39:41 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,1 +1,1 @@
-print"hello world"//This  python 2.7  and for you'll need add parenthesis                                
+print"hello world"//This  python 2.7  and for you'll need add parenthesis so it will  be print("hello world")
