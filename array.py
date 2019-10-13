68acfe19ac8f50c5738ce57c27c2252d74de57a7
DhanashriPanchputre <34817399+DhanashriPanchputre@users.noreply.github.com>
2019-10-13 22:16:38 +0300

    Create array.py

---
+++ array.py
@@ -0,0 +1,21 @@
+# Python Array Declaration
+
+# Declaring an empty Array
+array = []
+
+# Declaring an Integer Array
+int_array = [1, 2, 3, 4, 5, 6, 7, 8]
+
+# Declaring a String Array
+string_array = ['apple', 'banana', 'orange', 'kiwi']
+# Access Array Items
+
+# Declaring an Array
+array = [1, 2, 3, 4, 5]
+
+# Use Positive Index
+print("Item at Index Position 0 = ", array[0])
+print("Item at Index Position 1 = ", array[1])
+print("Item at Index Position 2 = ", array[2])
+print("Item at Index Position 3 = ", array[3])
+print("Item at Index Position 4 = ", array[4])
